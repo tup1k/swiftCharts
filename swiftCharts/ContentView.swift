d3db950f79c83710eededbe52eadb09fdbc4d67e
Oleg K <tup1k@yandex.ru>
2025-06-16 22:24:31 +0300

    Initial Commit

---
+++ swiftCharts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftCharts
+//
+//  Created by Олег Кор on 16.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftCharts/swiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftChartsApp.swift
+//  swiftCharts
+//
+//  Created by Олег Кор on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
